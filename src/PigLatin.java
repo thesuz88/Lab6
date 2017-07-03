1a1af8353a7dd16bc31b14f43b0a4bd0943236bc
thesuz88 <stevensuzio@gmail.com>
2017-07-03 10:59:00 -0400

    added ability to translate entire phrases

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -7,31 +7,40 @@ import java.util.Scanner;
 public class PigLatin {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String result;
-        String userWord;
+        String[] result = new String[50]; //had issues with scope when I didn't define array size
+        String userPhrase;
         String choice = "y";
-
+        
         //Display welcome message and prompt for word from user
         System.out.println("Welcome to the pig latin translator!\n");
 
         while (choice.equalsIgnoreCase("y")) {
-            System.out.println("Enter a word to be translated: ");
-            userWord = scan.nextLine();
+            System.out.println("Enter a word or phrase to be translated: ");
+            userPhrase = scan.nextLine();
 
             //convert to lowercase before translating
-            userWord = userWord.toLowerCase();
+            userPhrase = userPhrase.toLowerCase();
 
-            //isVowel will only work for one word translations
-            if (isVowel(userWord)) {
-                //vowels method
-                result = translateWithVowel(userWord);
-            } else {
-                //consonants method
-                result = translateWithConsonant(userWord);
+            //Split user input at spaces and assign to array
+            String[] splitSentence = userPhrase.split(" ");
+
+            System.out.println("Pig latin translation: ");
+
+            for (int i = 0; i < splitSentence.length; i++) {
+
+                //takes each input from array and checks pig latin conditions
+                if (isVowel(splitSentence[i])) {
+
+                    //vowels method taking elements from splitSentence and copies to result array
+                    result[i] = translateWithVowel(splitSentence[i]);
+                } else {
+
+                    //consonants method taking elements from splitSentence and copies to result array
+                    result[i] = translateWithConsonant(splitSentence[i]);
+                }
+                //prints out each element of result array for each iteration of the loop
+                System.out.print(result[i] + " ");
             }
-
-            System.out.println("Pig latin translation: " + result);
-
 
             System.out.println("\nWould you like to translate another word? (y/n)");
             choice = scan.nextLine();
@@ -40,38 +49,38 @@ public class PigLatin {
     }
 
     //Translates word starting with a consonant
-    public static String translateWithConsonant(String userWord) {
-        int wordLength = userWord.length();
+    public static String translateWithConsonant(String userSentence) {
+        int wordLength = userSentence.length();
         int firstVowelFound = 0;
 
-        //take all consonants up to the first vowel...How do i get program to stop at vowel?
+        //take all consonants up to the first vowel
         for (int i = 0; i < wordLength; i++) {
-            char v = userWord.charAt(i);
+            char v = userSentence.charAt(i);
             if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u') {
 
                 firstVowelFound = i;
                 break; //without this break the last vowel is moved to the beginning and word becomes scrambled
             }
         }
-        String startString = userWord.substring(firstVowelFound, wordLength);
-        String movedString = userWord.substring(0, firstVowelFound);
+        String startString = userSentence.substring(firstVowelFound, wordLength);
+        String movedString = userSentence.substring(0, firstVowelFound);
 
-        userWord = startString + movedString + "ay";
+        userSentence = startString + movedString + "ay";
 
-        return userWord;
+        return userSentence;
     }
 
     //Translates word starting with a vowel pig latin logic
-    public static String translateWithVowel(String userWord) {
+    public static String translateWithVowel(String userSentence) {
 
-        userWord = userWord.concat("way");
+        userSentence = userSentence.concat("way");
 
-        return userWord;
+        return userSentence;
     }
 
     //Determines if a word starts with a vowel or consonant
-    public static boolean isVowel(String userWord) {
-        char s = userWord.charAt(0);
+    public static boolean isVowel(String userSentence) {
+        char s = userSentence.charAt(0);
         if (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u') {
             return true;
         } else {
